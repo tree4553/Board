4c181bb88f7c7b7867e416060643dfb6a34cdcc5
Jeong-cheol Lee <tree4553@naver.com>
2022-10-17 23:46:50 +0900

    Posts Save

---
+++ src/main/java/com/board/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.board.service.posts;
+
+import com.board.domain.posts.PostsRepository;
+import com.board.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
